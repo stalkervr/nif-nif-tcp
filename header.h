6b160adee9eb6cacfe2a461168a46845dfff8714
stalkervr <stalkervr@gmail.com>
2020-07-08 09:54:58 +0700

    Add header.h

--- header.h
+++ header.h
@@ -5,4 +5,10 @@
 #ifndef NIF_NIF_TCP_HEADER_H
 #define NIF_NIF_TCP_HEADER_H
 
+// system lib connect
+#include <stdio.h>
+#include <pcap.h>
+//
+
 #endif //NIF_NIF_TCP_HEADER_H
+
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include "header.h"
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Hello new, World!\n");
     return 0;
 }
